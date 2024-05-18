bfe1515dd4571633acff10820ef3864e48cb4e33
enesarac <117079592+enesarac@users.noreply.github.com>
2024-05-18 20:06:41 +0300

    "C# Windows Form Copy Tool" // Kopylalama Aracı c#
    "C# Windows Form Copy Tool"

---
+++ kopyalama/kopyalama/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace kopyalama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKaynakSec = new System.Windows.Forms.Button();
+            this.btnHedefSec = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnKaynakSec
+            // 
+            this.btnKaynakSec.Location = new System.Drawing.Point(88, 12);
+            this.btnKaynakSec.Name = "btnKaynakSec";
+            this.btnKaynakSec.Size = new System.Drawing.Size(138, 36);
+            this.btnKaynakSec.TabIndex = 0;
+            this.btnKaynakSec.Text = "Kaynak Seç";
+            this.btnKaynakSec.UseVisualStyleBackColor = true;
+            this.btnKaynakSec.Click += new System.EventHandler(this.btnKaynakSec_Click);
+            // 
+            // btnHedefSec
+            // 
+            this.btnHedefSec.Location = new System.Drawing.Point(79, 82);
+            this.btnHedefSec.Name = "btnHedefSec";
+            this.btnHedefSec.Size = new System.Drawing.Size(156, 36);
+            this.btnHedefSec.TabIndex = 1;
+            this.btnHedefSec.Text = "Hedef Seç";
+            this.btnHedefSec.UseVisualStyleBackColor = true;
+            this.btnHedefSec.Click += new System.EventHandler(this.btnHedefSec_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(321, 151);
+            this.Controls.Add(this.btnHedefSec);
+            this.Controls.Add(this.btnKaynakSec);
+            this.Name = "Form1";
+            this.Text = "Kopya Aracı";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKaynakSec;
+        private System.Windows.Forms.Button btnHedefSec;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ kopyalama/kopyalama/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kopyalama
+{
+    public partial class Form1 : Form
+    {
+        private string kaynakDizin;
+        private string hedefDizin;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaynakSec_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "Seçilen Klasör";
+            openFileDialog1.Filter = "Klasörler|*.klasör";
+            openFileDialog1.ValidateNames = false;
+            openFileDialog1.CheckFileExists = false;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.Title = "Kaynak Dizini Seç";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                kaynakDizin = Path.GetDirectoryName(openFileDialog1.FileName);
+            }
+        }
+
+        private void btnHedefSec_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = Path.GetFileName(kaynakDizin) + ".klasör";
+            saveFileDialog1.Title = "Hedef Dizini Seç";
+            saveFileDialog1.ValidateNames = false;
+            saveFileDialog1.CheckFileExists = false;
+            saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.Filter = "Klasörler|*.klasör";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                hedefDizin = Path.GetDirectoryName(saveFileDialog1.FileName);
+                hedefDizin = Path.Combine(hedefDizin, Path.GetFileName(kaynakDizin));
+                DiziniKopyala(kaynakDizin, hedefDizin);
+                MessageBox.Show("Klasör başarıyla kopyalandı!");
+            }
+        }
+
+        private static void DiziniKopyala(string kaynak, string hedef)
+        {
+            DirectoryInfo kaynakBilgi = new DirectoryInfo(kaynak);
+            DirectoryInfo hedefBilgi = Directory.CreateDirectory(hedef);
+
+            foreach (FileInfo dosya in kaynakBilgi.GetFiles())
+            {
+                dosya.CopyTo(Path.Combine(hedefBilgi.FullName, dosya.Name), true);
+            }
+
+            foreach (DirectoryInfo dizin in kaynakBilgi.GetDirectories())
+            {
+                DiziniKopyala(dizin.FullName, Path.Combine(hedefBilgi.FullName, dizin.Name));
+            }
+           
+        }
+    }
+}
